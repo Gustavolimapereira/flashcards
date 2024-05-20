9d4ed3802df768636dac04b64b9885b17aee6af6
gustavolimapereira <gu.lim@hotmail.com>
2024-05-19 23:08:38 -0300

    flashcards

---
+++ src/app/data.js
@@ -0,0 +1,149 @@
+const palavras = [
+  {
+    palavra: 'Sehr gut, danke',
+    traducao: 'muito bem, obrigado',
+    pronunciacao: 'zêr gut, danka',
+  },
+  {
+    palavra: 'gut',
+    traducao: 'bem / bom',
+    pronunciacao: 'gut',
+  },
+  {
+    palavra: 'auch gut',
+    traducao: 'também bem',
+    pronunciacao: 'aukh gut',
+  },
+  {
+    palavra: 'suchen',
+    traducao: 'procurar',
+    pronunciacao: 'zúrren',
+  },
+  {
+    palavra: 'die Karte - Kärtchen',
+    traducao: 'cartão - cartãozinho',
+    pronunciacao: 'di kárta - kértien',
+  },
+  {
+    palavra: 'machen (ich mache, du machst)',
+    traducao: 'fazer',
+    pronunciacao: 'márren (ikh má-rre, du márrst)',
+  },
+  {
+    palavra: 'der Beruf',
+    traducao: 'profissão',
+    pronunciacao: 'déa be-rúf',
+  },
+  {
+    palavra: 'beruflich',
+    traducao: 'profissionalmente',
+    pronunciacao: 'be-rúflish',
+  },
+  {
+    palavra: 'das Gespräch',
+    traducao: 'conversa',
+    pronunciacao: 'das gesh-prékh',
+  },
+  {
+    palavra: 'der Student - die Studentin',
+    traducao: 'estudante',
+    pronunciacao: 'déa shtudent - di shtudentin',
+  },
+  {
+    palavra: 'der Schüler - die Schülerin',
+    traducao: 'aluno',
+    pronunciacao: 'déa shüler - di shülerin',
+  },
+  {
+    palavra: 'ich lerne Deutsch',
+    traducao: 'eu aprendo alemão',
+    pronunciacao: 'ikh lérne dóitch',
+  },
+  {
+    palavra: 'ich studiere Deutsch',
+    traducao: 'eu estudo alemão',
+    pronunciacao: 'ikh shtudí-rre dóitch',
+  },
+  {
+    palavra: 'das Praktikum',
+    traducao: 'estágio',
+    pronunciacao: 'das práctikum',
+  },
+  {
+    palavra: 'schreiben',
+    traducao: 'escrever',
+    pronunciacao: 'shráiben',
+  },
+  {
+    palavra: 'die Seite',
+    traducao: 'página / lado',
+    pronunciacao: 'di záita',
+  },
+  {
+    palavra: 'wir sind (nicht) verheiratet',
+    traducao: 'nós somos casados',
+    pronunciacao: 'vír zint (nisht) fe-rrairá-tet',
+  },
+  {
+    palavra: 'Familienstand',
+    traducao: 'estado civil',
+    pronunciacao: 'fa-mílien-shtand',
+  },
+  {
+    palavra: 'aber',
+    traducao: 'mas',
+    pronunciacao: 'áber',
+  },
+  {
+    palavra: 'leben',
+    traducao: 'viver',
+    pronunciacao: 'lében',
+  },
+  {
+    palavra: 'das Leben',
+    traducao: 'vida',
+    pronunciacao: 'das lében',
+  },
+  {
+    palavra: 'wieder',
+    traducao: 'novamente',
+    pronunciacao: 'ví-derr',
+  },
+  {
+    palavra: 'zusammen',
+    traducao: 'juntos',
+    pronunciacao: 'tsu-zámen',
+  },
+  {
+    palavra: 'Wir haben ein Kind',
+    traducao: 'nós temos uma criança',
+    pronunciacao: 'vír ráben áin kint',
+  },
+  {
+    palavra: 'das Kind - die Kinder',
+    traducao: 'criança',
+    pronunciacao: 'das kint - di kinder',
+  },
+  {
+    palavra: 'das Ei - die Eier',
+    traducao: 'ovo',
+    pronunciacao: 'das ai - di aier',
+  },
+  {
+    palavra: 'der Sohn',
+    traducao: 'filho',
+    pronunciacao: 'déa zon',
+  },
+  {
+    palavra: 'die Tochter',
+    traducao: 'filha',
+    pronunciacao: 'di tó-rre',
+  },
+  {
+    palavra: 'allein',
+    traducao: 'sozinho',
+    pronunciacao: 'a-láin',
+  },
+]
+
+export default palavras
