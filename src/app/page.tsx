58a952b5eeb06776901be40d82d0f8ab2708e96f
gustavolimapereira <gu.lim@hotmail.com>
2024-06-20 00:07:57 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,36 +21,75 @@ export default function Home() {
 
   return (
     <div className="bg-slate-500 w-full h-screen flex justify-center items-center">
-      <div className="h-[650px] w-[650px] bg-white border rounded-md justify-center flex flex-col items-center">
-        <div className="flex flex-row p-6">
-          <p className="m-1 p-2 font-bold">{currentWord.palavra}</p>
-          <div className="flex justify-center items-center">
-            <button
-              className="p-2 bg-blue-500 text-white rounded-md transform transition-transform duration-150 active:scale-95"
-              onClick={handleNewWord}
-            >
-              <ArrowPathIcon className="w-5 h-5" />
-            </button>
+      <div className="h-[450px] w-[550px] bg-white border rounded-md justify-center flex flex-col items-center">
+        <div className="flex flex-col p-2">
+          <p>Palavra</p>
+          <div className="flex flex-row ">
+            <form className="max-w-sm mx-auto ">
+              <input
+                type="text"
+                id="disable-input"
+                aria-label="disable input"
+                className=" bg-gray-100 border border-gray-300 text-black-600 font-bold text-sm rounded-lg block w-[300px] p-2.5 cursor-not-allowed "
+                value={currentWord.palavra}
+              ></input>
+            </form>
+            <div className="pl-2 flex justify-center items-center">
+              <button
+                className="p-2 bg-blue-500 text-white rounded-md transform transition-transform duration-150 active:scale-95"
+                onClick={handleNewWord}
+              >
+                <ArrowPathIcon className="w-5 h-5" />
+              </button>
+            </div>
           </div>
         </div>
 
-        <button
-          className="p-2 bg-blue-500 text-white rounded-md transform transition-transform duration-150 active:scale-95"
-          onClick={() => setShowTraducao((prev) => !prev)}
-        >
-          <ArrowPathIcon className="w-5 h-5" />
-        </button>
+        <div className="flex flex-col p-2">
+          <p>Tradução</p>
+          <div className="flex flex-row">
+            <form className="max-w-sm mx-auto">
+              <input
+                type="text"
+                id="disable-input"
+                aria-label="disable input"
+                className=" bg-gray-100 border border-gray-300 text-black-600 font-bold text-sm rounded-lg block w-[300px] p-2.5 cursor-not-allowed "
+                value={showTraducao ? currentWord.traducao : ''}
+              ></input>
+            </form>
+            <div className="pl-2 flex justify-center items-center">
+              <button
+                className="p-2 bg-blue-500 text-white rounded-md transform transition-transform duration-150 active:scale-95"
+                onClick={() => setShowTraducao((prev) => !prev)}
+              >
+                <ArrowPathIcon className="w-5 h-5" />
+              </button>
+            </div>
+          </div>
+        </div>
 
-        {showTraducao && <p>{currentWord.traducao}</p>}
-
-        <button
-          className="p-2 bg-blue-500 text-white rounded-md transform transition-transform duration-150 active:scale-95"
-          onClick={() => setShowPronuncia((prev) => !prev)}
-        >
-          <ArrowPathIcon className="w-5 h-5" />
-        </button>
-
-        {showPronuncia && <p>{currentWord.pronunciacao}</p>}
+        <div className="flex flex-col p-2">
+          <p>Pronúncia</p>
+          <div className="flex flex-row">
+            <form className="max-w-sm mx-auto">
+              <input
+                type="text"
+                id="disable-input"
+                aria-label="disable input"
+                className=" bg-gray-100 border border-gray-300 text-black-600 font-bold text-sm rounded-lg block w-[300px] p-2.5 cursor-not-allowed "
+                value={showPronuncia ? currentWord.pronunciacao : ''}
+              ></input>
+            </form>
+            <div className="pl-2 flex justify-center items-center">
+              <button
+                className="p-2 bg-blue-500 text-white rounded-md transform transition-transform duration-150 active:scale-95"
+                onClick={() => setShowPronuncia((prev) => !prev)}
+              >
+                <ArrowPathIcon className="w-5 h-5" />
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
